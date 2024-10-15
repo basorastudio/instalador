a85431ade7c3ad73a490dfe954a581c3b0699706
basorastudio <73445392+basorastudio@users.noreply.github.com>
2024-10-15 18:23:36 -0400

    Update _general.sh

--- variables/_general.sh
+++ variables/_general.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
-# Special variables to be used for general purposes.
+# Variables especiales a ser utilizadas para propósitos generales.
 
-readonly NC="\033[0m" # reset colors
+readonly NC="\033[0m" # reiniciar colores

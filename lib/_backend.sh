154a76256b7886cbcff9e0f7cb79fc7be7f98923
basorastudio <73445392+basorastudio@users.noreply.github.com>
2024-10-15 18:25:19 -0400

    Update _backend.sh

--- lib/_backend.sh
+++ lib/_backend.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # 
-# functions for setting up app backend
+# funciones para configurar el backend de la aplicación
 
 #######################################
-# creates docker db
-# Arguments:
-#   None
+# crea la base de datos en docker
+# Argumentos:
+#   Ninguno
 #######################################
 backend_db_create() {
   print_banner
-  printf "${WHITE} 💻 Criando banco de dados...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Creando base de datos...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -46,23 +46,23 @@ EOF
 }
 
 #######################################
-# sets environment variable for backend.
-# Arguments:
-#   None
+# configura las variables de entorno para el backend.
+# Argumentos:
+#   Ninguno
 #######################################
 backend_set_env() {
   print_banner
-  printf "${WHITE} 💻 Configurando variáveis de ambiente (backend)...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Configurando variables de entorno (backend)...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
 
-  # ensure idempotency
+  # asegurar idempotencia
   backend_url=$(echo "${backend_url/https:\/\/}")
   backend_url=${backend_url%%/*}
   backend_url=https://$backend_url
 
-  # ensure idempotency
+  # asegurar idempotencia
   frontend_url=$(echo "${frontend_url/https:\/\/}")
   frontend_url=${frontend_url%%/*}
   frontend_url=https://$frontend_url
@@ -80,7 +80,7 @@ ADMIN_DOMAIN=whazing.io
 PROXY_PORT=443
 PORT=3000
 
-# conexão com o banco de dados
+# conexión con la base de datos
 DB_DIALECT=postgres
 DB_PORT=5432
 DB_TIMEZONE=-04:00
@@ -89,48 +89,48 @@ POSTGRES_USER=whazing
 POSTGRES_PASSWORD=${pg_pass}
 POSTGRES_DB=postgres
 
-# Chaves para criptografia do token jwt
+# Claves para la encriptación del token jwt
 JWT_SECRET=${jwt_secret}
 JWT_REFRESH_SECRET=${jwt_refresh_secret}
 
-# Dados de conexão com o REDIS
+# Datos de conexión con REDIS
 IO_REDIS_SERVER=localhost
 IO_REDIS_PASSWORD=${redis_pass}
 IO_REDIS_PORT=6379
 IO_REDIS_DB_SESSION=2
 
-# tempo para randomização da mensagem de horário de funcionamento
+# tiempo para la randomización del mensaje de horario de funcionamiento
 MIN_SLEEP_BUSINESS_HOURS=1000
 MAX_SLEEP_BUSINESS_HOURS=2000
 
-# tempo para randomização das mensagens do bot
+# tiempo para la randomización de los mensajes del bot
 MIN_SLEEP_AUTO_REPLY=400
 MAX_SLEEP_AUTO_REPLY=600
 
-# tempo para randomização das mensagens gerais
+# tiempo para la randomización de los mensajes generales
 MIN_SLEEP_INTERVAL=200
 MAX_SLEEP_INTERVAL=500
 
-# dados do RabbitMQ / Para não utilizar, basta comentar a var AMQP_URL
+# datos de RabbitMQ / Para no utilizar, basta comentar la variable AMQP_URL
 RABBITMQ_DEFAULT_USER=whazing
 RABBITMQ_DEFAULT_PASS=${rabbit_pass}
 #AMQP_URL='amqp://whazing:${rabbit_pass}@localhost:5672?connection_attempts=5&retry_delay=5'
 
-# api oficial (integração em desenvolvimento)
+# api oficial (integración en desarrollo)
 API_URL_360=https://waba-sandbox.360dialog.io
 
-# usado para mosrar opções não disponíveis normalmente.
+# usado para mostrar opciones no disponibles normalmente.
 ADMIN_DOMAIN=whazing.io
 
-# Dados para utilização do canal do facebook
+# Datos para el uso del canal de facebook
 FACEBOOK_APP_ID=3237415623048660
 FACEBOOK_APP_SECRET_KEY=3266214132b8c98ac59f3e957a5efeaaa13500
 
-# Limitar Uso do whazing Usuario e Conexões
+# Limitar el uso de Whazing Usuario y Conexiones
 USER_LIMIT=999
 CONNECTIONS_LIMIT=999
 
-#config banco
+#configuración de la base de datos
 DB_DEBUG=false
 POSTGRES_POOL_MAX=100
 POSTGRES_POOL_MIN=10
@@ -145,13 +145,13 @@ EOF
 }
 
 #######################################
-# installs node.js dependencies
-# Arguments:
-#   None
+# instala dependencias de node.js
+# Argumentos:
+#   Ninguno
 #######################################
 backend_node_dependencies() {
   print_banner
-  printf "${WHITE} 💻 Instalando dependências do backend...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Instalando dependencias del backend...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -165,13 +165,13 @@ EOF
 }
 
 #######################################
-# compiles backend code
-# Arguments:
-#   None
+# compila el código del backend
+# Argumentos:
+#   Ninguno
 #######################################
 backend_node_build() {
   print_banner
-  printf "${WHITE} 💻 Compilando o código do backend...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Compilando el código del backend...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -185,13 +185,13 @@ EOF
 }
 
 #######################################
-# runs db migrate
-# Arguments:
-#   None
+# ejecuta la migración de la base de datos
+# Argumentos:
+#   Ninguno
 #######################################
 backend_db_migrate() {
   print_banner
-  printf "${WHITE} 💻 Executando db:migrate...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Ejecutando db:migrate...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -205,13 +205,13 @@ EOF
 }
 
 #######################################
-# runs db seed
-# Arguments:
-#   None
+# ejecuta la siembra de la base de datos
+# Argumentos:
+#   Ninguno
 #######################################
 backend_db_seed() {
   print_banner
-  printf "${WHITE} 💻 Executando db:seed...${GRAY_LIGHT}"
+  printf "${WHITE} 💻 Ejecutando db:seed...${GRAY_LIGHT}"
   printf "\n\n"
 
   sleep 2
@@ -225,10 +225,9 @@ EOF
 }
 
 #######################################
-# starts backend using pm2 in 
-# production mode.
-# Arguments:
-#   None
+# inicia el backend usando pm2 en modo producción.
+# Argumentos:
+#   Ninguno
 #######################################
 backend_start_pm2() {
   print_banner
@@ -247,9 +246,9 @@ EOF
 }
 
 #######################################
-# updates frontend code
-# Arguments:
-#   None
+# actualiza el código del frontend
+# Argumentos:
+#   Ninguno
 #######################################
 backend_nginx_setup() {
   print_banner
